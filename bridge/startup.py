7ea01c3379021bf9a4cc03fa1de23c0360c64c06
Dominic Zarro <dominic.zarro@nasa.gov>
2018-07-30 15:12:38 -0400

    Daily update

--- bridge/startup.py
+++ bridge/startup.py
@@ -2,6 +2,10 @@
 # PYTHON start-up file. Define PYTHONSTARTUP to point to this file
 
 print("Running Python-IDL bridge startup...")
-import bridge
-bridge.startup()
+try:
+    import bridge
+    bridge.startup()
+except:
+    pass
+
 from idlpy import * 
